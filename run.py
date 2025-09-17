f1d15cb85c908ec7034935f3fbec52a565747cd5
NoName <121759935+Noname-cybe@users.noreply.github.com>
2025-09-17 11:43:02 -0700

    Update run.py

--- run.py
+++ run.py
@@ -2,18 +2,14 @@ import os
 import subprocess
 import sys
 
-# Dapatkan direktori saat ini (folder script ini)
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Path lengkap ke main.exe di dalam folder dist/
 exe_path = os.path.join(base_dir, "dist", "main.exe")
 
-# Cek apakah file main.exe ada
 if not os.path.isfile(exe_path):
     print(f"❌ File main.exe tidak ditemukan di {exe_path}")
     sys.exit(1)
-
-# Jalankan main.exe
+    
 try:
     subprocess.run([exe_path], check=True)
 except subprocess.CalledProcessError as e:
